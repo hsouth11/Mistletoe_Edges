a658182b91bd3c8978ea116de3940d500cad1bf6
hsouth11 <hannosoutham@gmail.com>
2024-08-07 10:20:21 -0700

    Adding line to generate default token

--- scripts/git.R
+++ scripts/git.R
@@ -2,9 +2,13 @@
 #https://happygitwithr.com/https-pat
 
 library(gitcreds)
+library(usethis)
 
 #Run this, will show you current credetnials and give you an option to update it
 gitcreds_set()
 
 #Run this to check you've stored a token. Should say username: PersonalAccessToken, password: hidden
 gitcreds_get()
+
+#This line will open the chrome and the github page to generate a new token
+usethis::create_github_token()
