4c80bebab183da1358e624953391b4001049e613
hsouth11 <hannosoutham@gmail.com>
2024-08-21 10:31:54 -0700

    Added chunk to correct for differences in procedures used to measure tree height between us and the provincial ground sampling guidelines. We estimated length of apical droop. Prov guidelines measure to apex of apical droop.

--- scripts/cleaning and processing/extra height cleaning.R
+++ scripts/cleaning and processing/extra height cleaning.R
@@ -1,5 +1,9 @@
 #Code for cleaning extra regen height data
-#Hanno Southam, 7 Aug 2024
+#Hanno Southam
+#Date last updated: 21 Aug 2024
+#Date last ran: 21 Aug 2024
+
+rm(list=ls(all=TRUE))
 
 #Load package
 library(tidyverse)
@@ -12,13 +16,27 @@ summary(ht_hdm_extra)
 ht_hdm_extra <- ht_hdm_extra %>% filter(!is.na(site_id))
 
 #Problem 2: a number of trees are missing crown class
-#Our protocol was to only measure codominant and dominant trees
+#All of these extra trees were codominant and dominants
 #Assume these were codominants
 ht_hdm_extra <- ht_hdm_extra %>% 
   mutate(crown_class = if_else(is.na(crown_class), "C", crown_class))
 
 #Problem 3: dbh measurements are missing for the trees at ph_3 but there is
 #no easy way to remedy that. 
+
+#Problem 4: The protocol we used to measure height differed from the 
+#provincial ground sampling procedures. We added height to accommodate the 
+#apical droop of Hw. Prov procedures measure to apex of apical droop. 
+#Correct by subtracting standard amount for each component/crown class.
+#Because all of these trees are regen and codominant/dominants, some of these 
+#rules don't have cases in this data. But included all rules because this 
+#correction also happens in script for cleaning larger tree dataset. 
+#Keeping it consistent. 
+#Mature: if codominant/dominant = 1m; intermediate = 0.75, suppressed = 0.5m
+#Regen: codominant = 0.5m; intermediate = 0.4m, suppressed = 0.3m
+ht_hdm_extra <- ht_hdm_extra %>% 
+  mutate(ht_corr = height_m - 0.5)
+
 
 #Export this csv as a clean dataset
 # write_csv(ht_hdm_extra, "./data/cleaned/regen_extra_ht_c.csv")
