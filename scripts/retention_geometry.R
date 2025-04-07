75888456aa82bcb0ba28aeedb1daa30a5365aec1
hsouth11 <hannosoutham@gmail.com>
2025-04-07 14:45:26 -0700

    Changed zone of infelunce where growth impacts within a rotation may occur from 20 to 15 m.

--- scripts/retention_geometry.R
+++ scripts/retention_geometry.R
@@ -2,6 +2,8 @@ library(sf)
 library(tmap)
 library(dplyr)
 
+rm(list=ls(all=TRUE))
+
 #Step 1: Create a 16 ha cutblock (400m x 400m)
 cb <- st_as_sf(st_sfc(st_polygon(list(rbind(
   c(0, 0), c(400, 0), c(400, 400), c(0, 400), c(0, 0)))), crs = 3005))
@@ -42,12 +44,12 @@ tmap_mode("plot") +
   tm_shape(rp) +
     tm_borders(col = "green")
 
-#Step 4: Create 20 and 35m buffers around retention patches
+#Step 4: Create 15 and 35m buffers around retention patches
 #Restrict to area within the cutblock and remove the retention patches 
 #themselves
 rp_35 <- st_buffer(rp, 35) %>% 
   st_intersection(cb)
-rp_20 <- st_buffer(rp, 20) %>% 
+rp_15 <- st_buffer(rp, 15) %>% 
   st_intersection(cb)
 
 #Step 5: Create the same buffers around the block edge
@@ -56,20 +58,20 @@ rp_20 <- st_buffer(rp, 20) %>%
 ed_35 <- st_buffer(cb_line, 35) %>% 
   st_intersection(cb) %>% 
   st_difference(st_combine(rp))
-ed_20 <- st_buffer(cb_line, 20) %>% 
+ed_15 <- st_buffer(cb_line, 15) %>% 
   st_intersection(cb) %>% 
   st_difference(st_combine(rp))
 
 #Plot again to check
-#20m buffers
+#15m buffers
 tmap_mode("plot") +
   tm_shape(cb) +
     tm_borders(col = "black", lwd = 2) +
   tm_shape(rp) +
     tm_fill(col = "darkgreen", alpha = 0.3) +
-  tm_shape(ed_20) +
+  tm_shape(ed_15) +
     tm_borders(col = "blue", lty = "dashed")+
-  tm_shape(rp_20) +
+  tm_shape(rp_15) +
     tm_borders(col = "green", lty = "dashed") +
   tm_layout(frame = F)
 
@@ -88,7 +90,7 @@ tmap_mode("plot") +
 #Step 6:Remove overlapping areas
 #Remove internal overlaps from retention patch buffers and the 
 #retention patches themselves
-rp_20 <- st_union(rp_20) %>% 
+rp_15 <- st_union(rp_15) %>% 
   st_difference(st_combine(rp))
 rp_35 <- st_union(rp_35) %>% 
   st_difference(st_combine(rp))
@@ -98,29 +100,29 @@ rp_35 <- st_union(rp_35) %>%
 #objects, one that identifies these areas of dual overlap and one that removes
 #these areas
 #Identify areas of overlap
-overlap_20 <- rp_20 %>% 
-  st_intersection(ed_20)
+overlap_15 <- rp_15 %>% 
+  st_intersection(ed_15)
 overlap_35 <- rp_35 %>% 
   st_intersection(ed_35)
 
 #Areas of difference
-rp_20 <- rp_20 %>% 
-  st_difference(ed_20)
+rp_15 <- rp_15 %>% 
+  st_difference(ed_15)
 rp_35 <- rp_35 %>% 
   st_difference(ed_35)
 
 #Plot to make sure that worked
-#20m buffers
+#15m buffers
 tmap_mode("plot") +
   tm_shape(cb) +
     tm_borders(col = "black", lwd = 2) +
   tm_shape(rp) +
     tm_fill(col = "darkgreen", alpha = 0.3) +
-  tm_shape(rp_20) +
+  tm_shape(rp_15) +
     tm_borders(col = "green", lty = "dashed") +
-  tm_shape(ed_20) +
+  tm_shape(ed_15) +
     tm_borders(col = "blue", lty = "dashed") +
-  tm_shape(overlap_20)+
+  tm_shape(overlap_15)+
     tm_fill(col = "grey", alpha = 0.3) + 
   tm_layout(frame = F)
 
@@ -152,39 +154,41 @@ a_rp
 #Area within each buffer
 a_35 <- st_union(ed_35, rp_35) %>% st_area()/10000
 a_35
-a_20<- st_union(ed_20, rp_20) %>% st_area()/10000
-a_20
+a_15<- st_union(ed_15, rp_15) %>% st_area()/10000
+a_15
 
 #What % of harvested area is within a tree length in this case? 
 a_35/a_cb*100
 
 #What % of harvested area is within 20m?
-a_20/a_cb*100
+a_15/a_cb*100
 
 #How much of this is additional to what is in the cutblock without retention?
-ed_20_clearcut <- st_buffer(cb_line, 20) %>% 
+ed_15_clearcut <- st_buffer(cb_line, 15) %>% 
   st_intersection(cb)
-a_ed_20_clearcut <- st_area(ed_20_clearcut)/10000
+a_ed_15_clearcut <- st_area(ed_15_clearcut)/10000
 #Compare these two numbers
-a_ed_20_clearcut
-a_20 #Approximately doubles it
-a_20 - a_ed_20_clearcut
+a_ed_15_clearcut
+a_15 #Approximately doubles it
+a_15 - a_ed_15_clearcut
 
 #How much is within the area affected by two edges? 
 #This isn't quite accurate right now because it doesn't incporporate area 
-#within 20m of two retention patches. But from the plots, these are minimal,
+#within 15m of two retention patches. But from the plots, these are minimal,
 #so its a good approximation
-a_overlap <- st_area(overlap_20)/10000
+a_overlap <- st_area(overlap_15)/10000
 a_overlap
 
 #Assuming 30% of mature edge infected, how much of harevested area affected?
 #These are "independent" probabilities, but have to account for overlapping
 #areas by including them twice
-a_inf <- ((st_area(rp_20)*0.3) + 
-  (st_area(ed_20)*0.3) + (st_area(overlap_20)*0.3))/10000
+a_inf <- ((st_area(rp_15)*0.3) + 
+  (st_area(ed_15)*0.3) + (st_area(overlap_15)*0.3))/10000
+a_inf
 
 #If just a clearcut
-a_inf_clearcut <- a_ed_20_clearcut*0.3
+a_inf_clearcut <- a_ed_15_clearcut*0.3
+a_inf_clearcut
 
 #Difference
 a_inf - a_inf_clearcut
@@ -215,8 +219,8 @@ ha_35 <- st_as_sf(ha_35)
 rp <- st_as_sf(rp)
 rp_35 <- st_as_sf(rp_35)
 ed_35 <- st_as_sf(ed_35)
-rp_20 <- st_as_sf(rp_20)
-ed_20 <- st_as_sf(ed_20)
+rp_15 <- st_as_sf(rp_15)
+ed_15 <- st_as_sf(ed_15)
 
 #Combine all polygons into one sf object with a category attribute
 group_reten <- bind_rows(
@@ -225,13 +229,14 @@ group_reten <- bind_rows(
   rp %>% mutate(category = "Mature forest"),
   rp_35 %>% mutate(category = "≤ 35 m"),
   ed_35 %>% mutate(category = "≤ 35 m"),
-  rp_20 %>% mutate(category = "≤ 20 m"),
-  ed_20 %>% mutate(category = "≤ 20 m")) %>% 
+  rp_15 %>% mutate(category = "≤ 15 m"),
+  ed_15 %>% mutate(category = "≤ 15 m")) %>% 
   mutate(category = factor(category, levels = c("Mature forest", "> 35 m",
-                                                "≤ 35 m", "≤ 20 m")),
+                                                "≤ 35 m", "≤ 15 m")),
          system = "Group retention")
-#
-ed_20_clearcut <- st_buffer(cb_line, 20) %>% 
+
+#Create a clearcut to plot
+ed_15_clearcut <- st_buffer(cb_line, 15) %>% 
   st_intersection(cb)
 ed_35_clearcut <- st_buffer(cb_line, 35) %>% 
   st_intersection(cb)
@@ -242,9 +247,9 @@ clearcut <- bind_rows(
   edge %>% mutate(category = "Mature forest"),
   ha_35_clearcut %>% mutate(category = "> 35 m"),
   ed_35_clearcut %>% mutate(category = "≤ 35 m"),
-  ed_20_clearcut %>% mutate(category = "≤ 20 m")) %>% 
+  ed_15_clearcut %>% mutate(category = "≤ 15 m")) %>% 
   mutate(category = factor(category, levels = c("Mature forest", "> 35 m",
-                                                "≤ 35 m", "≤ 20 m")),
+                                                "≤ 35 m", "≤ 15 m")),
          system = "Clearcut")
 
 x <- bind_rows(clearcut,
