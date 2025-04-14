9ebc33ce85d03067da3e730c326536c23f803c5a
hsouth11 <hannosoutham@gmail.com>
2025-04-14 14:41:59 -0700

    Removing scripts/area harvested BC.R from Git tracking but keeping locally.

--- scripts/area harvested BC.R
+++
@@ -1,8 +0,0 @@
-#Summary of area harvested in BC
-#Original data: Canadian Council of Forest Ministers. (2023). Forest area harvested on private and Crown lands in Canada [dataset]. National Forestry Database. http://nfdp.ccfm.org/en/data/harvest.php
-### NEED TO CHECK THIS IS JUST FOR PUBLIC LAND IN BC
-
-library("tidyverse")
-har <- read_csv("/Users/hannosoutham/OneDrive - UBC (1)/Msc/Grants and Awards/Wall Scholarship/area harvested.csv")
-
-summary(har)
